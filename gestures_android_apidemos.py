3b5a128291dca467ca619341850948263436f16a
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2023-12-19 00:02:51 -0800

    Create gestures_android_apidemos.py

---
+++ gestures_android_apidemos.py
@@ -0,0 +1,43 @@
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.interaction import POINTER_TOUCH
+from selenium.webdriver.common.actions.mouse_button import MouseButton
+
+APP = 'https://github.com/appium/android-apidemos/releases/download/v3.1.0/ApiDemos-debug.apk'
+
+APPIUM = 'http://localhost:4723'
+CAPS = {
+    "platformName": "Android",
+    "appium:options": {
+        "platformVersion": "14.0",      # optional
+        "deviceName": "Android Emulator",
+        "automationName": "UiAutomator2",
+        'app': APP
+    }
+}
+
+driver = webdriver.Remote(APPIUM, CAPS)
+try:
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located(
+        (AppiumBy.ACCESSIBILITY_ID, 'Views'))).click()
+    wait.until(EC.presence_of_element_located(
+        (AppiumBy.ACCESSIBILITY_ID, 'Animation')))
+
+    print(driver.get_window_size())     # {'width': 1080, 'height': 2201}
+
+    actions = ActionBuilder(driver)
+    finger = actions.add_pointer_input(POINTER_TOUCH, 'finger')
+    finger.create_pointer_move(duration=0, x=100, y=1600)    # move_to_start
+    finger.create_pointer_down(button=MouseButton.LEFT)    # press_down
+    finger.create_pointer_move(duration=250, x=0, y=-1600, origin='pointer')    # move_to_end
+    finger.create_pointer_up(button=MouseButton.LEFT)      # press_up
+    actions.perform()
+
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Visibility')
+    driver.get_screenshot_as_file('screencap.png')
+finally:
+    driver.quit()
